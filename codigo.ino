068cb4ccff3005de85852515c088306f2bd88aed
Ana Clara Gonçalves <78920638+sun-moo@users.noreply.github.com>
2022-11-03 10:34:43 -0300

    Update codigo.ino

--- codigo.ino
+++ codigo.ino
@@ -1,11 +1,17 @@
 #define va 2 
 #define m1A 3 
 #define m2A 4 
+#define va2 5
+#define me1 6
+#define me2 7
 
 void setup() {
   pinMode(va, OUTPUT); 
   pinMode(m1A, OUTPUT); 
   pinMode(m2A, OUTPUT); 
+  pinMode(va2, OUTPUT);
+  pinMode(me1, OUTPUT);
+  pinMode(me2, OUTPUT);
 
   Serial.begin(9600); 
 }
@@ -19,6 +25,22 @@ void loop() {
         digitalWrite(m2A, LOW); 
         digitalWrite(va, 255); 
       break; 
+        
+      case 'B':
+        traz();
+      break;
+      
+      case 'R':
+        viraDireita();
+      break;
+        
+      case 'L':
+        viraEsquerda();
+      break;
+        
+      case 'S':
+        para();
+      break;
     }
   }
 }
