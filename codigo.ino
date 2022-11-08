038222e2dab0cbab212fa5d48ab38fd6acafc0ea
Ana Clara Gonçalves <78920638+sun-moo@users.noreply.github.com>
2022-11-08 12:53:31 -0300

    Update codigo.ino

--- codigo.ino
+++ codigo.ino
@@ -1,17 +1,17 @@
-#define va 2 
-#define m1A 3 
-#define m2A 4 
-#define va2 5
-#define me1 6
-#define me2 7
+#define VMA 2 
+#define M1A 3 
+#define M2A 4 
+#define VMB 5
+#define M1B 6
+#define M2B 7
 
 void setup() {
-  pinMode(va, OUTPUT); 
-  pinMode(m1A, OUTPUT); 
-  pinMode(m2A, OUTPUT); 
-  pinMode(va2, OUTPUT);
-  pinMode(me1, OUTPUT);
-  pinMode(me2, OUTPUT);
+  pinMode(VMA, OUTPUT); 
+  pinMode(M1A, OUTPUT); 
+  pinMode(M2A, OUTPUT); 
+  pinMode(VMB, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2B, OUTPUT);
 
   Serial.begin(9600); 
 }
@@ -21,13 +21,21 @@ void loop() {
     char valorRecebido = Serial.read(); 
     switch(valorRecebido){ 
       case 'F':
-        digitalWrite(m1A, HIGH);
-        digitalWrite(m2A, LOW); 
-        digitalWrite(va, 255); 
+        digitalWrite(M1A, HIGH);
+        digitalWrite(M2A, LOW); 
+        digitalWrite(VMA, 255); 
+        digitalWrite(M1B, HIGH);
+        digitalWrite(M2B, LOW);
+        digitalWrite(VMB, 255);
       break; 
         
       case 'B':
-        traz();
+        digitalWrite(M1A, LOW);
+        digitalWrite(M2A, HIGH); 
+        digitalWrite(VMA, 255); 
+        digitalWrite(M1B, LOW);
+        digitalWrite(M2B, HIGH);
+        digitalWrite(VMB, 255);
       break;
       
       case 'R':
